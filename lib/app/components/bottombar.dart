0f1064fb656cd1df8a38a5e21e63db76dc32dd73
Disezmike <mikenatchapon@gmail.com>
2024-02-27 10:32:27 +0700

    .

--- lib/app/components/bottombar.dart
+++ lib/app/components/bottombar.dart
@@ -10,41 +10,44 @@ class MyBottomBar extends StatelessWidget {
   final void Function(int)? onTabChange;
 
   final List list = [
-    {"icon" : Icons.home, "text": "หน้าแรก"},
-    {"icon" : Icons.storefront, "text": "หน้าร้านค้า"},
-    {"icon" : Icons.menu, "text": "เมนู"},
+    {"icon": Icons.home, "text": "หน้าแรก"},
+    {"icon": Icons.storefront, "text": "หน้าร้านค้า"},
+    {"icon": Icons.menu, "text": "เมนู"},
   ];
 
   @override
   Widget build(BuildContext context) {
     return Container(
-          // color: Colors.white,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            boxShadow: const [
-              BoxShadow(
-              color: Colors.grey,
-              offset: Offset(0, 5),
-              blurRadius: 10,
-              spreadRadius: 5,
-              )
-            ]
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding/2, vertical: kDefaultPadding),
-            child: GNav(
-                onTabChange: onTabChange,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                // backgroundColor: Colors.deepOrange,
-                color: Colors.grey.shade500,
-                activeColor: Colors.deepOrange.shade500,
-                tabActiveBorder: Border.all(color: Colors.deepOrange.shade500),
-                tabBackgroundColor: Colors.white,
-                gap: 10,
-                padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding*1.5, vertical: kDefaultPadding),
-                tabs: list.map((e) => GButton(icon: e["icon"], text: e['text'], textStyle: gNavTextStyle,)).toList()
-                ),
-          ),
-        );
+      // color: Colors.white,
+      decoration: BoxDecoration(color: Colors.white, boxShadow: const [
+        BoxShadow(
+          color: Colors.grey,
+          offset: Offset(0, 5),
+          blurRadius: 10,
+          spreadRadius: 5,
+        )
+      ]),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+            horizontal: kDefaultPadding / 2, vertical: kDefaultPadding),
+        child: GNav(
+        onTabChange: onTabChange,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        // backgroundColor: Colors.deepOrange,
+        color: Colors.grey.shade500,
+        activeColor: Colors.deepOrange.shade500,
+        tabActiveBorder: Border.all(color: Colors.deepOrange.shade500),
+        tabBackgroundColor: Colors.white,
+        gap: 10,
+        padding: const EdgeInsets.symmetric(
+          horizontal: kDefaultPadding * 1.5, vertical: kDefaultPadding),
+        tabs: list.map((e) => GButton(
+          icon: e["icon"],
+          text: e['text'],
+          textStyle: gNavTextStyle,
+        )).toList()
+      ),
+      ),
+    );
   }
 }
