83f6029aa4747098c1df18f9aa7095c43dee5774
Disezmike <mikenatchapon@gmail.com>
2024-04-01 00:14:15 +0700

    edit login page

--- lib/app/screens/login/login.dart
+++ lib/app/screens/login/login.dart
@@ -4,7 +4,6 @@
 
 // ignore_for_file: avoid_print
 
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -41,100 +40,109 @@ class _MyLoginPageState extends State<MyLoginPage> {
   void initState() {
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Google Sign In'),
-        ),
         body: ConstrainedBox(
-          constraints: const BoxConstraints.expand(),
-          child: buildBody(),
-        ));
+      constraints: const BoxConstraints.expand(),
+      child: buildBody(),
+    ));
   }
 
-void _handdleSignInWithGoogle() async {
-  setState(() {
-    _isLoading = true;
-  });
-  try {
-    var account = await Authentication.signInWithGoogle();
+  void _handdleSignInWithGoogle() async {
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      var account = await Authentication.signInWithGoogle();
 
-    bool isAuthorized = account != null;
-    if (account != null) {
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      prefs.setBool('auth', isAuthorized);
+      bool isAuthorized = account != null;
+      if (account != null) {
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        prefs.setBool('auth', isAuthorized);
 
-      User user = User(email: account.email, photoUrl: account.photoURL, displayName: account.displayName);
-      FirebaseFirestore database = FirebaseFirestore.instance;
-      CollectionReference ref = database.collection('users');
-      ref.where("email", isEqualTo: user.email!).get().then((value) => value.docs).then((value) async {
-        if (value.isNotEmpty) {
-          await prefs.setString('userId', value[0].id);
-        } else {
-          dynamic data = user.toJson();
-          data['role'] = ['user'];
-          try {
-            final dRef = await ref.add(data);
-            data['id'] = dRef.id;
-            await ref.doc(dRef.id).update(data);
-            await prefs.setString('userId', dRef.id);
+        User user = User(
+            email: account.email,
+            photoUrl: account.photoURL,
+            displayName: account.displayName);
+        FirebaseFirestore database = FirebaseFirestore.instance;
+        CollectionReference ref = database.collection('users');
+        ref
+            .where("email", isEqualTo: user.email!)
+            .get()
+            .then((value) => value.docs)
+            .then((value) async {
+          if (value.isNotEmpty) {
+            await prefs.setString('userId', value[0].id);
+          } else {
+            dynamic data = user.toJson();
+            data['role'] = ['user'];
+            try {
+              final dRef = await ref.add(data);
+              data['id'] = dRef.id;
+              await ref.doc(dRef.id).update(data);
+              await prefs.setString('userId', dRef.id);
 
-            print(data);
-          } catch (e) {
-            print(e);
+              print(data);
+            } catch (e) {
+              print(e);
+            }
           }
-        }
+        });
+      }
+
+      if (isAuthorized) {
+        context.pushReplacement('/home');
+      }
+    } catch (e) {
+      setState(() {
+        _isLoading = false;
+        error = "มีปัญหาในการเข้าสู้ระบบโปรลองอีกครั้ง";
       });
     }
-    
-    if (isAuthorized) {
-      context.pushReplacement('/home');
-    }
-  } catch (e) {
-    setState(() {
-      _isLoading = false;
-      error = "มีปัญหาในการเข้าสู้ระบบโปรลองอีกครั้ง";
-    });
   }
-}
 
   Widget buildBody() {
     return Center(
-    child: _isLoading ? const Loading() : Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        SignInButton(
-          buttonType: ButtonType.google,
-          onPressed: _handdleSignInWithGoogle),
-          // ...[
-          //   const SizedBox(height: kDefaultPadding),
-          //   SignInButton(
-          //     buttonType: ButtonType.googleDark,
-          //     btnText: "Developer mode",
-          //     onPressed: _handdleToDevMode
-          //   )
-          // ],
-          ...[
-            const SizedBox(height: kDefaultPadding),
-            Text("$error",style: GoogleFonts.kanit(textStyle: const TextStyle(fontSize: kDefaultFontSize*1.2, color: Colors.red)),)
-          ]
-      ],
-    ),
-  );
-}
-
-  void _handdleToDevMode() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setBool('auth', true);
-    prefs.setString('userId', "U6JSoJYVVvXqpf4IdqtPCXy1GN92");
-    context.pushReplacement('/home');
-  }
-
-  void test() {
-    FirebaseFirestore.instance.collection('shops').doc('1').collection('orders').orderBy('queue', descending: false).get().then((docs) {
-      var data = docs.docs.map((e) => [e.data()['queue'], (e.data()['createdAt'] as Timestamp).toDate()]);
-      print(data);
-    });
+      child: _isLoading
+          ? const Loading()
+          : Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                // Center(
+                //   child: Text(
+                //     "เข้าสู่ระบบด้วย",
+                //     style: GoogleFonts.kanit(
+                //       textStyle: const TextStyle(
+                //         fontSize: kDefaultFontSize*1.5
+                //       )
+                //     ),
+                //   ),
+                // ),
+                const SizedBox(
+                  height: kDefaultPadding,
+                ),
+                SignInButton(
+                  buttonType: ButtonType.google,
+                  // buttonSize: ButtonSize.small,
+                  width: 300,
+                  padding: kDefaultPadding,
+                  btnText: "ดำเนินการต่อด้วย Google",
+                  onPressed: _handdleSignInWithGoogle,
+                ),
+                ...[
+                  const SizedBox(height: kDefaultPadding),
+                  Text(
+                    "$error",
+                    style: GoogleFonts.kanit(
+                        textStyle: const TextStyle(
+                            fontSize: kDefaultFontSize * 1.2,
+                            color: Colors.red)),
+                  )
+                ]
+              ],
+            ),
+    );
   }
 }
